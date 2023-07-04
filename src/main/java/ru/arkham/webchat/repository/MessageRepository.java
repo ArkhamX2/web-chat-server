3a8352061685ebedcdbb017d846c19792cd19380
Alexander <106086600+dubstepTractor@users.noreply.github.com>
2023-07-04 11:10:24 +0300

    update message repository

--- src/main/java/ru/arkham/webchat/repository/MessageRepository.java
+++ src/main/java/ru/arkham/webchat/repository/MessageRepository.java
@@ -1,8 +1,8 @@
 package ru.arkham.webchat.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.arkham.webchat.model.Chat;
 import ru.arkham.webchat.model.Message;
+import ru.arkham.webchat.model.MessageStatus;
 
 import java.util.Optional;
 
@@ -17,4 +17,24 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
      * @return сообщение или ничего.
      */
     Optional<Message> findByChatId(Long chatId);
+
+    /**
+     * Подсчитать количество сообщений от определенного отправителя
+     * в определенном чате и с определенным статусом.
+     * @param senderId идентификатор отправителя.
+     * @param chatId идентификатор чата.
+     * @param status статус отправки.
+     * @return количество сообщений.
+     */
+    Long countBySenderIdAndChatIdAndStatus(Long senderId, Long chatId, MessageStatus status);
+
+    /**
+     * Обновить статусы всех сообщений от определенного отправителя
+     * в определенном чате.
+     * TODO: Протестировать.
+     * @param senderId идентификатор отправителя.
+     * @param chatId идентификатор чата.
+     * @param status статус отправки.
+     */
+    void updateAllBySenderIdAndChatId(Long senderId, Long chatId, MessageStatus status);
 }
