aaba68820afb03c7a8bd529329caa1a397390131
Исламов Мурад Гюльмуса оглы <99683993+S1stemus@users.noreply.github.com>
2023-07-05 09:00:26 +0300

    решено
    найти способ на jpa реализовать такой запрос: UPDATE message SET status = '?' WHERE senderId = '?' AND chatId = '?';

--- src/main/java/ru/arkham/webchat/repository/MessageRepository/MessageRepo.java
+++ src/main/java/ru/arkham/webchat/repository/MessageRepository/MessageRepo.java
@@ -2,11 +2,13 @@ package ru.arkham.webchat.repository.MessageRepository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.arkham.webchat.model.Messanger.Message;
+import ru.arkham.webchat.model.User;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface MessageRepo extends JpaRepository<Message,String> {
         List<Message> findByChatId(String chatId);
+        List<Message> findByChatIdAndUserID(String ChatId, String UserId);
 
 }
--- src/main/java/ru/arkham/webchat/service/MessageService/MessageService.java
+++ src/main/java/ru/arkham/webchat/service/MessageService/MessageService.java
@@ -1,4 +1,22 @@
 package ru.arkham.webchat.service.MessageService;
 
+import org.apache.tomcat.util.http.parser.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.arkham.webchat.model.Messanger.Message;
+import ru.arkham.webchat.model.Messanger.MessageStatus;
+import ru.arkham.webchat.model.User;
+import ru.arkham.webchat.repository.MessageRepository.MessageRepo;
+
+import java.util.List;
+
 public class MessageService {
+    @Autowired
+    MessageRepo messageRepo;
+    private void updateStatus(MessageStatus status, User user, String ChatId){
+        List<Message> messageList= messageRepo.findByChatIdAndUserID( ChatId,user.getId().toString());
+        for (Message message:messageList ) {
+            message.setStatus(status);
+            messageRepo.save(message);
+        }
+    }
 }
